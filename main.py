62377fac84dcf1315b21dac2af202279fd27d2db
arthurkehrwald <arthur.kehrwald@gmail.com>
2024-02-28 14:51:06 +0100

    Simplify syntax

--- main.py
+++ main.py
@@ -66,7 +66,9 @@ def convert_to_cv_frame(data: dai.ADatatype) -> cv2.typing.MatLike:
     return mat_like
 
 
-def try_get_img(img_q: dai.DataOutputQueue) -> typing.Tuple[bool, cv2.typing.MatLike | None]:
+def try_get_img(
+    img_q: dai.DataOutputQueue,
+) -> typing.Tuple[bool, cv2.typing.MatLike | None]:
     if img_q.has():
         return True, convert_to_cv_frame(img_q.get())
     return False, None
@@ -74,19 +76,21 @@ def try_get_img(img_q: dai.DataOutputQueue) -> typing.Tuple[bool, cv2.typing.Mat
 
 def try_get_centroid(
     img: cv2.typing.MatLike, threshold01: float, show_preview: bool, preview_name: str
-) -> typing.Tuple[bool, int, int]:
+) -> typing.Tuple[bool, float, float]:
     ret, thresh = cv2.threshold(img, 254 * threshold01, 255, 0)
     M = cv2.moments(thresh)
     if M["m00"] == 0.0:
         return False, -1, -1
-    cX = int(M["m10"] / M["m00"])
-    cY = int(M["m01"] / M["m00"])
+    cX = M["m10"] / M["m00"]
+    cY = M["m01"] / M["m00"]
     if show_preview:
-        cv2.circle(img, (cX, cY), 5, (255, 0, 0), -1)
+        circle_pos = (int(cX), int(cY))
+        cv2.circle(img, circle_pos, 5, (255, 0, 0), -1)
+        text_pos = (int(cX), int(cY) - 25)
         cv2.putText(
             img,
             "centroid",
-            (cX - 25, cY - 25),
+            text_pos,
             cv2.FONT_HERSHEY_SIMPLEX,
             0.5,
             (255, 255, 255),
@@ -97,12 +101,15 @@ def try_get_centroid(
     return True, cX, cY
 
 
-def DLT(proj_l, proj_r, point_l, point_r) -> np.ndarray:
+def DLT(
+    proj_l: np.ndarray,
+    proj_r: np.ndarray,
+    point_l: typing.Tuple[float, float],
+    point_r: typing.Tuple[float, float],
+) -> np.ndarray:
     # cv.triangulatePoints operates on lists of points
-    points_l = np.zeros((2, 1))
-    points_l[:, 0] = point_l
-    points_r = np.zeros((2, 1))
-    points_r[:, 0] = point_r
+    points_l = np.array(point_l)
+    points_r = np.array(point_r)
     points4d: np.ndarray = cv2.triangulatePoints(proj_r, proj_l, points_r, points_l)
     first = points4d[:, 0]
     first = first[:3] / first[3]  # homogenous -> cartesian
@@ -139,13 +146,13 @@ with dai.Device(pipeline) as device:
             success_r, cX_r, cY_r = try_get_centroid(
                 img_r, threshold01=threshold01, show_preview=True, preview_name="right"
             )
-        if success_l and success_r:
-            tracked_pos = DLT(p_r, p_l, [cX_r, cY_r], [cX_l, cY_r])
-            print(tracked_pos)
-            tracked_pos_with_empty_rotation = np.zeros(6)
-            tracked_pos_with_empty_rotation[:3] = tracked_pos
-            tracked_pos_bytes = tracked_pos_with_empty_rotation.tobytes()
-            sock.sendto(tracked_pos_bytes, (IP, PORT))
+            if success_l and success_r:
+                tracked_pos = DLT(p_r, p_l, (cX_r, cY_r), (cX_l, cY_r))
+                print(tracked_pos)
+                tracked_pos_with_empty_rotation = np.zeros(6)
+                tracked_pos_with_empty_rotation[:3] = tracked_pos
+                tracked_pos_bytes = tracked_pos_with_empty_rotation.tobytes()
+                sock.sendto(tracked_pos_bytes, (IP, PORT))
         key = cv2.waitKey(1)
         if key == ord("+"):
             threshold01 = min(threshold01 + 0.05, 0.99)

cee54e28a5490e423be39595f8c0ec8821da7e88
arthurkehrwald <arthur.kehrwald@gmail.com>
2026-07-21 12:33:07 +0200

    Split centroid estimation and preview

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ The stereo node is shared, so it must be created before and linked
 
 class MonoPipeline:
     def __init__(
-        self, pipeline: dai.Pipeline, stereo_node: dai.node.StereoDepth, is_left: bool
+            self, pipeline: dai.Pipeline, stereo_node: dai.node.StereoDepth, is_left: bool
     ) -> None:
         self.pipeline = pipeline
         self.stereo_node = stereo_node
@@ -67,7 +67,7 @@ class MonoPipeline:
 
 
 def try_get_centroid(
-    img: cv2.typing.MatLike, threshold01: float, show_preview: bool, preview_name: str
+        img: cv2.typing.MatLike, threshold01: float
 ) -> typing.Tuple[bool, float, float]:
     ret, thresh = cv2.threshold(img, 254 * threshold01, 255, 0)
     M = cv2.moments(thresh)
@@ -75,10 +75,16 @@ def try_get_centroid(
         return False, -1, -1
     cX = M["m10"] / M["m00"]
     cY = M["m01"] / M["m00"]
-    if show_preview:
-        circle_pos = (int(cX), int(cY))
+    return True, cX, cY
+
+
+def show_preview(img: cv2.typing.MatLike, name: str, found_centroid: bool, centroid_x: float, centroid_y: float):
+    if found_centroid:
+        cx = int(centroid_x)
+        cy = int(centroid_y)
+        circle_pos = (cx, cy)
         cv2.circle(img, circle_pos, 5, (255, 0, 0), -1)
-        text_pos = (int(cX), int(cY) - 25)
+        text_pos = (cx, cy - 25)
         cv2.putText(
             img,
             "centroid",
@@ -88,16 +94,14 @@ def try_get_centroid(
             (255, 255, 255),
             2,
         )
-        cv2.imshow(preview_name, img)
-        cv2.imshow(preview_name + " thresh", thresh)
-    return True, cX, cY
+    cv2.imshow(name, img)
 
 
 def DLT(
-    proj_l: np.ndarray,
-    proj_r: np.ndarray,
-    point_l: typing.Tuple[float, float],
-    point_r: typing.Tuple[float, float],
+        proj_l: np.ndarray,
+        proj_r: np.ndarray,
+        point_l: typing.Tuple[float, float],
+        point_r: typing.Tuple[float, float],
 ) -> np.ndarray:
     # cv.triangulatePoints operates on lists of points
     points_l = np.array(point_l)
@@ -106,6 +110,7 @@ def DLT(
     first = points4d[:, 0]
     first = first[:3] / first[3]  # homogenous -> cartesian
     return first
+
 
 with dai.Pipeline() as pipeline:
     stereo_depth = pipeline.create(dai.node.StereoDepth)
@@ -122,7 +127,7 @@ with dai.Pipeline() as pipeline:
     pipeline_l.set_exposure(200, 100)
     p_l = pipeline_l.get_projection_matrix()
     p_r = pipeline_r.get_projection_matrix()
-    threshold01 = 0.5
+    threshold01 = 0.9
 
     with pipeline:
         while pipeline.isRunning():
@@ -130,11 +135,12 @@ with dai.Pipeline() as pipeline:
             success_r, img_r = pipeline_r.try_get_img()
             if success_l and success_r and img_l is not None and img_r is not None:
                 success_l, cX_l, cY_l = try_get_centroid(
-                    img_l, threshold01=threshold01, show_preview=True, preview_name="left"
+                    img_l, threshold01=threshold01
                 )
                 success_r, cX_r, cY_r = try_get_centroid(
-                    img_r, threshold01=threshold01, show_preview=True, preview_name="right"
+                    img_r, threshold01=threshold01
                 )
+                show_preview(img_l, "Point Tracker", success_l, cX_l, cY_l)
                 if success_l and success_r:
                     tracked_pos = DLT(p_r, p_l, (cX_r, cY_r), (cX_l, cY_r))
                     print(tracked_pos)
@@ -142,6 +148,7 @@ with dai.Pipeline() as pipeline:
                     tracked_pos_with_empty_rotation[:3] = tracked_pos
                     tracked_pos_bytes = tracked_pos_with_empty_rotation.tobytes()
                     sock.sendto(tracked_pos_bytes, (IP, PORT))
+
             key = cv2.waitKey(1)
             if key == ord("+"):
                 threshold01 = min(threshold01 + 0.05, 0.99)
